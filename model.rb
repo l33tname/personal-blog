c9d19a6edab2ccc040267f1136cefde3979049b5
fliiiix <de-ch@hotmail.de>
2017-10-01 15:56:17 +0200

    rewriten model code

--- model.rb
+++ model.rb
@@ -1,9 +1,19 @@
-require 'active_model/serializers'
-require "mongo_mapper"
+Sequel::Model.plugin :timestamps
 
-class Post
-	include MongoMapper::Document
+# create_table! Drop the table if exists, and then create the table
+# create_table? Create the table only if it doesn't exists.
+DB.create_table? :posts do
+  primary_key :id
+  String :text, text: true
+  DateTime :created_at
+  DateTime :updated_at
+end
 
-	key :text, String, :require => true
-	timestamps!
+class Post < Sequel::Model(:posts)
+end
+
+Post.dataset_module do
+  def all_sorted
+        Post.reverse_order(:created_at)
+    end
 end
