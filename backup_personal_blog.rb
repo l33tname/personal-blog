ea631d9f810a6feee9e137f73b904a3e0bd4f7b5
fliiiix <de-ch@hotmail.de>
2018-05-12 16:04:09 +0200

    fixed backup script

--- backup_personal_blog.rb
+++ backup_personal_blog.rb
@@ -2,8 +2,10 @@ require_relative "model.rb"
 
 MongoMapper.setup({'production' => {'uri' => ENV['MONGODB_BLOG_URI']}}, 'production')
 
+SQL_DATE_FORMATER = "%Y-%m-%d %H:%M:%S"
+
 for p in Post.all
     puts "INSERT INTO posts (text, created_at, updated_at, url)
- VALUES ('#{p.text}', #{p.created_at.strftime('%Y%m%d %I:%M:%S %p')}, #{p.updated_at.strftime('%Y%m%d %I:%M:%S %p')}, '#{p._id}')"
+ VALUES ('#{p.text.gsub("'", "\\\\'")}', '#{p.created_at.strftime(SQL_DATE_FORMATER)}', '#{p.updated_at.strftime(SQL_DATE_FORMATER)}', '#{p._id}');"
 end
 
